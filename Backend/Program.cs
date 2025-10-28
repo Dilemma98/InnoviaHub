d1fa46d0e86b638dbfd155eb70c4a5090976f158
Emma Högdal <emmaah98@outlook.com>
2025-10-28 11:44:16 +0100

    added github.io to cors

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactDev", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "https://dilemma98.github.io/InnoviaHub/")
+        policy.WithOrigins("http://localhost:5173", "https://dilemma98.github.io")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
