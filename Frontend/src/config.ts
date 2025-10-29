14db9cd894e52c85bd0cdae6f8186d5d0ba6aea9
Emma Högdal <emmaah98@outlook.com>
2025-10-29 13:26:54 +0100

    added /api/ to baseURL

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -1,5 +1,5 @@
 // For live
-export const BASE_URL = "https://innoviahub-backend-had9greebjazakbe.swedencentral-01.azurewebsites.net/";
+export const BASE_URL = "https://innoviahub-backend-had9greebjazakbe.swedencentral-01.azurewebsites.net/api/";
 
 // For development
 // export const BASE_URL = "http://localhost:5271/api/";
