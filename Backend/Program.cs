7774edf816c6cd8824dd53ce86c6b8aa7f69a753
Emma Högdal <emmaah98@outlook.com>
2025-11-18 08:29:49 +0100

    changed pipeline for useCors

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -370,8 +370,8 @@ using (var scope = app.Services.CreateScope())
 //   PIPELINE
 // =======================
 
-app.UseRouting();
 app.UseCors("AllowReactDev");
+app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
