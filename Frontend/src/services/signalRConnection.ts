3c3f73da788b02d2ea8f1e6a1dfce21ede0ee140
Emma Högdal <emmaah98@outlook.com>
2025-11-20 10:08:55 +0100

    effing cors

--- Frontend/src/services/signalRConnection.ts
+++ Frontend/src/services/signalRConnection.ts
@@ -2,15 +2,16 @@ import * as signalR from "@microsoft/signalr";
 
 const isDev = import.meta.env.DEV;
 
-const hubUrl = isDev 
-  ? "http://localhost:5271/bookinghub" 
+const hubUrl = isDev
+  ? "http://localhost:5271/bookinghub"
   : "https://innoviahub-backend-had9greebjazakbe.swedencentral-01.azurewebsites.net/bookinghub";
-
 
 console.log("----🔌 SignalR ansluter till:------", hubUrl);
 
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl(hubUrl)
+  .withUrl(hubUrl, {
+    withCredentials: true,
+  })
   .withAutomaticReconnect()
   .build();
 
